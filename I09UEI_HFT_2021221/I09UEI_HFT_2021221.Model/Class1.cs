de42d885960cca4b010e97a6b50d60a3355e98db
Hamza Unsal <30478627+Neymaro@users.noreply.github.com>
2021-10-12 22:32:52 +0200

    Sending my created project

---
+++ I09UEI_HFT_2021221/I09UEI_HFT_2021221.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace I09UEI_HFT_2021221.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ I09UEI_HFT_2021221/I09UEI_HFT_2021221.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace I09UEI_HFT_2021221.Repository
+{
+    public class Class1
+    {
+    }
+}
