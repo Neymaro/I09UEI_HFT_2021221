fa147b38f3ecbd3542e35a116851a523a0861f6b
Hamza Unsal <30478627+Neymaro@users.noreply.github.com>
2021-10-12 23:12:26 +0200

    Update Class1.cs

--- I09UEI_HFT_2021221/I09UEI_HFT_2021221.Data/Class1.cs
+++ I09UEI_HFT_2021221/I09UEI_HFT_2021221.Data/Class1.cs
@@ -2,7 +2,12 @@
 
 namespace I09UEI_HFT_2021221.Data
 {
+
+    //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\ProjectDB.mdf;Integrated Security=True
+
+
     public class Class1
     {
-    }
+
+        }
 }
